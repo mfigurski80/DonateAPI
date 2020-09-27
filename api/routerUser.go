30a2cdb5ea4c7324c819bca60971c93634312444
Mikolaj Figurski <meek.f80@gmail.com>
2020-09-27 01:17:23 -0400

    Add user update path

--- api/routerUser.go
+++ api/routerUser.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/mfigurski80/DonateAPI/store"
 )
 
 // GET `/{userId}`
@@ -16,7 +17,7 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if user.Username != userID {
-		respondUnauthorized(w, "You are not authorized to view this page")
+		respondUnauthorized(w, "You are not authorized to access this page")
 		return
 	}
 
@@ -27,6 +28,44 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write(bytes)
+}
+
+// PUT `/{userId}`
+func putUser(w http.ResponseWriter, r *http.Request) {
+	// auth
+	userID := mux.Vars(r)["userId"]
+	user, ok := authRequest(w, r)
+	if !ok {
+		return
+	}
+	if user.Username != userID {
+		respondUnauthorized(w, "You are not authorized to access this page")
+		return
+	}
+
+	// read body
+	var postUser postUserStruct
+	ok = unmarshalRequestBody(w, r, postUser)
+	if !ok {
+		return
+	}
+
+	// implement changes
+	users, err := store.ReadUsers()
+	if err != nil {
+		respondInternalServerError(w, err)
+		return
+	}
+	user.Password = store.HashPassword(postUser.Password)
+	(*users)[user.Username] = user
+	err = store.WriteUsers(users)
+	if err != nil {
+		respondInternalServerError(w, err)
+		return
+	}
+
+	// respond
+	w.Write(makeSuccessResponse("user password has been updated", user.Username))
 }
 
 func addUserSubrouter(r *mux.Router) {
